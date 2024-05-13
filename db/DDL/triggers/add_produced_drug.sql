4801cabd7bb304580ce3107dfc13ba9647cd0bff
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-13 17:54:46 +0700

    Пофиксил некоторые триггеры и добавил триггеры в отчёт

--- db/DDL/triggers/add_produced_drug.sql
+++ db/DDL/triggers/add_produced_drug.sql
@@ -3,7 +3,7 @@ create definer = llirik42@`%` trigger add_produced_drug
     on production
     for each row
 begin
-    if (new.end is not null) then
+    if (old.end is null and new.end is not null) then
         # if everything is ok, create record in storage
         select drug_id into @drug_id from technologies where technologies.id = new.technology_id;
         insert into storage_items (drug_id, available_amount, original_amount, receipt_datetime) values (@drug_id, new.drug_amount, new.drug_amount, now());
--- db/DDL/triggers/check_no_techs_for_not_cookable.sql
+++ db/DDL/triggers/check_no_techs_for_not_cookable.sql
@@ -3,5 +3,7 @@ create definer = llirik42@`%` trigger check_no_techs_for_not_cookable
     on drug_types
     for each row
 begin
-    call check_no_technologies_for_drug_type(new.id);
+    if (not new.cookable and old.cookable) then
+        call check_no_technologies_for_drug_type(new.id);
+    end if;
 end;
